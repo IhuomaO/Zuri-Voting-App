360b4374a7b75a38eae0cc2e265ef79450ae679c
Samuel Osinloye <psalmueloye@gmail.com>
2022-04-21 10:55:43 +0100

    Get connect account

--- react-app/src/context/IndigoVotingContext.js
+++ react-app/src/context/IndigoVotingContext.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { ethers } from "ethers";
+import { toast } from "react-toastify";
 
 import { contractABI, contractAddress } from "utils/constants";
 
@@ -20,8 +21,38 @@ const getEthereumContract = () => {
 };
 
 export const IndigoVotingProvider = ({ children }) => {
+  const [currentAccount, setCurrentAccount] = useState("");
+  const chechIfWalletIsConnected = async () => {
+    try {
+      if (!ethereum) return toast.warning("Please install metamask");
+
+      const accounts = await ethereum.request({ method: "eth_accounts" });
+      if (accounts.length) {
+        setCurrentAccount(accounts[0]);
+      }
+      console.log(accounts);
+    } catch (error) {
+      toast.warning(`${error.response}`);
+    }
+  };
+
+  const connectWallet = async () => {
+    try {
+      if (!ethereum) return toast.warning("Please install metamask");
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      setCurrentAccount(accounts[0]);
+    } catch (error) {
+      toast.warning(`${error.response}`);
+    }
+  };
+  useEffect(() => {
+    chechIfWalletIsConnected();
+  }, []);
+
   return (
-    <IndigoVotingContext.Provider value={{}}>
+    <IndigoVotingContext.Provider value={{ connectWallet, currentAccount }}>
       {children}
     </IndigoVotingContext.Provider>
   );
