dd31a80408e422f9f2c4d63ae84b5f6a03584df8
Samuel Osinloye <psalmueloye@gmail.com>
2022-04-21 22:30:04 +0100

    Created private route

--- react-app/src/utils/constants.js
+++ react-app/src/utils/constants.js
@@ -1,3 +1,3 @@
-// import Abi from "contracts/contract.json";
-export const contractAddress = "";
-export const contractABI = "";
+import Abi from "./IndigoVoting.json";
+export const contractAddress = "0x05f53180ddf192c06609d1c63828a123429acc16";
+export const contractABI = Abi;
